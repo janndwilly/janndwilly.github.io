6dcd2d18621291e2feb27f5252886f99b12885e4
Janya <janndw8@gmail.com>
2025-08-29 12:58:39 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,24 +27,33 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
-
+    toggleGrid();
 
     // TODO 2 - Create Platforms
-
-
-
+    createPlatform(200, 650, 200, 20, "yellow");
+    createPlatform(300, 550, 200, 20, "orange");
+    createPlatform(500, 425, 200, 20, "yellow");
+    createPlatform(500, 320, 150, 10, "orange");
+    createPlatform(250, 200, 100, 10, "red");
+    createPlatform(550, 150, 75, 10, "yellow");
+    createPlatform(890, 350, 75, 10, "orange");
+    createPlatform(750, 600, 350, 20, "red");
+    createPlatform(1000, 450, 300, 10, "green");
+    createPlatform(700, 180, 5, 300, "blue");
+    createPlatform(50, 500, 75, 10, "green");
+    createPlatform(125, 200, 5, 300, "blue");
+    createPlatform(705, 265, 75, 10, "yellow");
 
     // TODO 3 - Create Collectables
-
-
-
-    
+    createCollectable("max", 275, 160);
+    createCollectable("diamond", 1050, 410);
+    createCollectable("steve", 1140, 410);
+    createCollectable("kennedi", 1230, 410);
+    createCollectable("max", 125, 125);
     // TODO 4 - Create Cannons
-
-
-    
-    
+    createCannon("right", 630, 550)
+    createCannon("top", 870, 600)
+    createCannon("bottom", 950, 600)
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
